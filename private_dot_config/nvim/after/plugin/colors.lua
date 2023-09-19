4824c84c65d0446a969ec6e06fa478aa8590ab3c
ahong75 <ahong75@gatech.edu>
2023-09-19 17:00:10 -0400

    Update .config/nvim/after/plugin/colors.lua

--- private_dot_config/nvim/after/plugin/colors.lua
+++ private_dot_config/nvim/after/plugin/colors.lua
@@ -1,2 +1,4 @@
-vim.cmd([[colorscheme gruvbox-material]])
+vim.g.gruvbox_material_background = 'soft'
+vim.g.gruvbox_material_better_performance = 1
 vim.o.background = "light" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox-material]])
