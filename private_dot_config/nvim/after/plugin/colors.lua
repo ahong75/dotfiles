685121014b02002c22595c46e485771e7c5cd147
ahong75 <ahong75@gatech.edu>
2023-09-20 01:28:22 -0400

    Remove .config/nvim/after/plugin/bufferline.lua Remove .config/nvim/after/plugin/colors.lua Remove .config/nvim/after/plugin/fugitive.lua Remove .config/nvim/after/plugin/lsp.lua Remove .config/nvim/after/plugin/lualine.lua Remove .config/nvim/after/plugin/telescope.lua Remove .config/nvim/after/plugin/treesitter.lua Remove .config/nvim/after/plugin/vimtex.lua Remove .config/nvim/init.lua Remove .config/nvim/lua/flyed/init.lua Remove .config/nvim/lua/flyed/packer.lua Remove .config/nvim/lua/flyed/remap.lua Remove .config/nvim/lua/flyed/set.lua Remove .config/nvim/plugin/packer_compiled.lua

--- private_dot_config/nvim/after/plugin/colors.lua
+++
@@ -1,5 +0,0 @@
-vim.g.gruvbox_material_background = 'soft'
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_statusline_style = 'default'
-vim.o.background = "light" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox-material]])
--- private_dot_config/nvim/lua/flyed/init.lua
+++
@@ -1,6 +0,0 @@
-require("flyed.remap")
-require("flyed.set")
-
-
-
-
